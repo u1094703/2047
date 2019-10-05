b8b2bfd890c0095f34ac4882d1b5d91adccc2a61
u1094703 <u1094703@utah.edu>
2019-10-05 17:39:49 -0600

    added Enemy, finished Character

--- Character.py
+++ Character.py
@@ -1,1 +1,20 @@
-import pygame
+class Character:
+    def __init__  (self, maxHP = 20, currentHP = 20, attack = 0, defense = 0, critical = 0, positionX = 2, positionY = 4):
+        self.maxHP = maxHP
+        self.attack = attack
+        self.defense = defense
+        self.critical = critical
+        self.positionX = positionX
+        self.positionY = positionY
+
+    def scoot_right (self):
+        self.positionX += 1
+
+    def scoot_left (self):
+        self.positionX -= 1
+
+    def scoot_up (self):
+        self.positionY -= 1
+
+    def scoot_down (self):
+        self.positionY += 1
