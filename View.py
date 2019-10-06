ae6c777a5b56785ed15efa967014a378ff221430
Krishna Patel <Krishna84106@gmail.com>
2019-10-05 21:27:53 -0600

    Working on view

--- View.py
+++ View.py
@@ -1,9 +1,17 @@
 import pygame
 pygame.init()
-screen=pygame.display.set_mode((400,400))
+pygame.display.set_caption("2047")
+background=pygame.image.load('/Users/Krish/Desktop/color.jpg')
+
+screen = pygame.display.set_mode((400,400))
+
+pygame.display.update()
 isBlue=True
 done=False
 while not done:
+    screen.blit(background, (400, 400))
+
+    pygame.display.update()
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             done =True
